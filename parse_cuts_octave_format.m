caf9e277cda9cc01be3491a1f34b3e7f7512ab94
aiolasimone <saiola@princeton.edu>
2017-02-08 02:19:03 +0200

    New cuts format added to parse_cuts_octave_format.m

--- parse_cuts_octave_format.m
+++ parse_cuts_octave_format.m
@@ -2,10 +2,9 @@ function[n_samp,samp_offset]=parse_cuts_octave_format(tod,lines,varargin)
 max_prime=get_keyval_default('max_prime',0,varargin{:});
 offset=get_keyval_default('cuts_offset',0,varargin{:});
 
-
 j=1;
 while (strcmp(strtrim(lines{j}),'END')==0)
-  eval([lines{j} ';']);
+  eval([lines{j} ';'])
   j=j+1;
 end
 if max_prime>0,
@@ -22,19 +21,27 @@ if isempty(tod)
 end
 
 jstart=j+1;
-strip_tag=' rc:(),';
+if (format_version==1),
+  strip_tag=' rc:(),';
+end
+if (format_version==2),
+  strip_tag=' :(),';
+end
 
 [rr,cc]=get_tod_rowcol(tod);
+dd=cc+rr*32;
 found_cuts=false(size(rr));
 for j=jstart:length(lines),
-  det_cuts=cellstr2mat(mystrsplit(lines{j},strip_tag,true));
-  myind=find((det_cuts(2)==rr)&(det_cuts(3)==cc));
+  det_cuts=cellstr2mat(mystrsplit(lines{j},strip_tag,true));  
+  myind=find(det_cuts(1)==dd);
   if ~isempty(myind)
     found_cuts(myind)=true;
-    myrow=det_cuts(2);
-    mycol=det_cuts(3);
-    if length(det_cuts)>3 %if nothing, it isn't cut
-      mycuts=det_cuts(4:end);
+    dd(myind);
+    myrow=rr(myind);
+    mycol=cc(myind);
+    if (((format_version==1)&&(length(det_cuts)>3))||((format_version==2)&&(length(det_cuts)>1))) %if nothing, it isn't cut
+      if (format_version==1), mycuts=det_cuts(4:end);end
+      if (format_version==2), mycuts=det_cuts(2:end);end
       if (mycuts(1)==0)&&(mycuts(2)>=n_samp)
         cut_detector_c(tod,myrow,mycol);
       else
@@ -44,8 +51,8 @@ for j=jstart:length(lines),
       end
     end
   end
-
 end
+             
 rr_bad=rr(found_cuts==false);
 cc_bad=cc(found_cuts==false);
 if (~isempty(rr_bad))
